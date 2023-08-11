6690d7d2820dfa5094528eb804d910ed910676c3
AbdullahJMubarak <abdalluh.moubarak@gmail.com>
2023-08-11 16:48:00 +0300

    writing c codes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{		char s;
+		int i;
+		long int li;
+		long long int lli;
+		float f;
+
+		printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(s));
+		printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
+		printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
+		printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lii));
+		printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+		return (0);
+}
